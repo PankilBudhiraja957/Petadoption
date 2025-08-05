b6ea151fe1196c2f2de0fe4ef100bd0327715fd8
Pankil <budhirajapankil89@gmail.com>
2025-08-05 10:53:24 +0530

    Add files via upload

---
+++ static/script1.js
@@ -0,0 +1,55 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
+}
+window.addEventListener('scroll', e => {
+  document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+
+
+/**
+ * header active when window scrolled down
+ */
+
+const header = document.querySelector("[data-header]");
+
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50 ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
